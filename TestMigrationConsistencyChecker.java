3a887f1e77d6604ce69cf7aa6c061c464c9f6644
Roy Saliba <rsaliba95@icloud.com>
2018-03-28 13:35:52 -0400

    Added file to prepare test for data migration

---
+++ TestMigrationConsistencyChecker.java
@@ -0,0 +1,22 @@
+package org.springframework.samples.petclinic.owner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestMigrationConsistencyChecker {
+
+	@Test
+	public void testMigrationConsistency() {
+		
+	}
+}
