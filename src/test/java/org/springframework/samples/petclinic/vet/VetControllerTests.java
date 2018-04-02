0dc48d289e48ef1e80b957387807424cfb4e19eb
tusmanakhter <tusman96@hotmail.com>
2018-04-02 01:26:50 -0400

    Fixed some test

--- src/test/java/org/springframework/samples/petclinic/vet/VetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetControllerTests.java
@@ -17,10 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.samples.petclinic.vet.Specialty;
-import org.springframework.samples.petclinic.vet.Vet;
-import org.springframework.samples.petclinic.vet.VetController;
-import org.springframework.samples.petclinic.vet.VetRepository;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -37,7 +33,10 @@ public class VetControllerTests {
 
     @MockBean
     private VetRepository vets;
-
+    
+    @MockBean
+    private VetService vetService;
+    
     @Before
     public void setup() {
         Vet james = new Vet();
