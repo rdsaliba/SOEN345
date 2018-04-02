6e3dc6a411a6e72fe4e45df4bcacb9f2aeef54e9
Tajbid Choudhury <tajbidhussain@hotmail.com>
2018-04-01 23:45:50 -0400

    Added update table switch statement consistency checker

--- src/main/java/org/springframework/samples/petclinic/database/ConsistencyChecker.java
+++ src/main/java/org/springframework/samples/petclinic/database/ConsistencyChecker.java
@@ -1,6 +1,8 @@
 package org.springframework.samples.petclinic.database;
 
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 import org.springframework.samples.petclinic.database.HashData;
 import org.springframework.samples.petclinic.database.HashGenerationException;
 
@@ -9,28 +11,43 @@ public class ConsistencyChecker {
     //private static Logger log = LoggerFactory.getLogger(ConsistencyChecker.class);
 	private String oldData [][];
 	private String newData [][];
-
+	private int	errorOccurance;
+	private int totalRowChecked;
+	
 	public ConsistencyChecker(String oldData [][],String newData [][]) {
 		this.oldData=oldData;
 		this.newData=newData;
 	}
 
-	public void checkConsistency() throws HashGenerationException {
-
+	public void checkConsistency(String Table) throws HashGenerationException {
+		errorOccurance = 0;
+		totalRowChecked = 0;
 		for (int i=0; i<oldData.length;i++)
 		{
+			totalRowChecked++;
 			for (int j=0; j<oldData[0].length;j++) {
 				String data_old = HashData.getHashFromString(oldData[i][j]);
 				String data_new = HashData.getHashFromString(newData[i][j]);
 				if (!(data_old.equals(data_new))) {
 					System.out.println("Failed Migration");
 					System.out.println(data_old + " not same as " + data_new);
-					//newData[i][j]= oldData[i][j];
-					//updateData(newData[i][j]);
+					
+					switch (Table) {
+					case "owners"			: break;
+					case "pets"				: break;
+					case "specialties"		: break;
+					case "vets"				: break;
+					case "visits"			: break;
+					case "vet_specialties"	: break;
+					}
 				}
 			}
 		}
 		System.out.println("CHECK COMPLETE");
+	}
+	
+	public void thresholdCheck() {
+		System.out.println();
 	}
 
 	public void updateData() {}
